f2dc48dccb36f6560ca7c4f1cc393accd2d97336
aynzo <106069925+aynzo@users.noreply.github.com>
2022-05-23 11:34:20 +0200

    Alexander Yakoub
    Kod för temperatursensor läsning

--- Matl_da_Arduino.ino
+++ Matl_da_Arduino.ino
@@ -1,13 +1,24 @@
-int i = 0;
-float temp = 0;
+#define sensorPin A0 //Definierad som analog input 0
 
 void setup() {
+  // Hur snabbt kommunikationen mellan Arduino och datorn kommer vara. Just nu är den på default som är Baud rate 9600:
   Serial.begin(9600);
 }
 
 void loop() {
-  i = analogRead(A0); // analog pin A0
-  temp = (i/1024.0)*500;
-  Serial.println(temp);
-  delay(1000);
+  //läsning från analog input:
+  int reading = analogRead(sensorPin);
+
+  // Ge läsningen ett float värde:
+  float x = reading * (5000 / 1024.0);
+
+  // Konvertera värdet till celsius genom att ta det genom 10:
+  float temperature = x / 10;
+
+  // Sedan printar man ut temperaturen i serial kommandon:
+  Serial.print(temperature);
+  Serial.print(" \xC2\xB0"); // Visar celsius symbolen:
+  Serial.println("C");
+
+  delay(1000); // Hur länge det dröjer tills nästa temp läsning:
 }
